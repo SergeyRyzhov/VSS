f2a7ceb93c50d290b2e36039262ed7cbc347aa0c
FirstExile <FirstExile@list.ru>
2013-10-16 21:55:14 +0400

    Тестовый проект

---
+++ Buddy.All/Buddy.Drawer/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисуем линию
+            e.Graphics.DrawLine(System.Drawing.Pens.Green, 10, 10, 150, 150);
+        }
+
+    }
+}
