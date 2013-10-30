05e1fe50323df905bc54c26f9664b817b9aafbe0
SergeyRyzhov <serega.rf@gmail.com>
2013-10-31 00:05:52 +0400

    Added interfaces for reduce and for neighborhood

---
+++ Buddy.All/Buddy.Common/Structures/IReducible.cs
@@ -0,0 +1,10 @@
+﻿namespace Buddy.Common.Structures
+{
+    public interface IReducible
+    {
+        /// <summary>
+        /// Получение редуцированного графа
+        /// </summary>
+        IGraph Reduce(uint[] labels);
+    }
+}
