6ccf116e0b52fc810e8b5adefb0082adeccf0598
SergeyRyzhov <serega.rf@gmail.com>
2013-11-02 22:23:21 +0400

    Array Coordinates was added

---
+++ Buddy.All/Buddy.Common/Structures/ArrayCoordinate.cs
@@ -0,0 +1,27 @@
+﻿namespace Buddy.Common.Structures
+{
+    public class ArrayCoordinate
+    {
+        private readonly double[] m_x;
+        private readonly double[] m_y;
+
+        public ArrayCoordinate(double[] x, double[] y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        public Coordinate this[int index]
+        {
+            get
+            {
+                return new Coordinate(m_x[index], m_y[index]);
+            }
+            set
+            {
+                m_x[index] = value.X;
+                m_y[index] = value.Y;
+            }
+        }
+    }
+}
