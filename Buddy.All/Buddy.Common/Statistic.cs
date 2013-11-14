503e13ee3eb520268b30012821f9518102083fca
AnastasiaBorisovaB <aya9draga@mail.ru>
2013-11-14 17:47:26 +0400

    Статистика для пересечения кругов

--- Buddy.All/Buddy.Common/Statistic.cs
+++ Buddy.All/Buddy.Common/Statistic.cs
@@ -43,6 +43,24 @@ namespace Buddy.Common
         {
             //TODO реализовать расчёт площади пересечения кругов
             return Math.PI*r1*r1 + Math.PI*r2*r2;
+            double f1, f2,d,s1,s2,s;
+            d = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            if (d == r1)
+            {
+                s = Math.PI * r1 * r1;
+            }
+            if (d == r2)
+            {
+                s = Math.PI * r2 * r2;
+            }
+            if (d < r1+r2)
+            {
+                f2 = 2 * Math.Acos((Math.Pow(r1, 2) - Math.Pow(r2, 2) + Math.Pow(d, 2)) / (2 * r1 * d));
+                f1 = 2 * Math.Acos((Math.Pow(r2, 2) - Math.Pow(r1, 2) + Math.Pow(d, 2)) / (2 * r2 * d));
+                s1 = Math.Pow(r1, 2) * (f1 - Math.Sin(f1)) / 2;
+                s2 = Math.Pow(r2, 2) * (f2 - Math.Sin(f2)) / 2;
+                s = s1 + s2;
+            }
         }
 
         private static double Distance(double x1, double y1, double x2, double y2)
