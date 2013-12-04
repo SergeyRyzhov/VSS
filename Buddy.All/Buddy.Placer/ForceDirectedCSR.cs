805c695c75069de81b33b63faae6863634205414
SergeyRyzhov <serega.rf@gmail.com>
2013-12-04 13:07:00 +0400

    Cleanup

--- Buddy.All/Buddy.Placer/ForceDirectedCSR.cs
+++ Buddy.All/Buddy.Placer/ForceDirectedCSR.cs
@@ -6,15 +6,14 @@ using Buddy.Common.Structures;
 
 namespace Buddy.Placer
 {
-   public class ForceDirectedCSR : BasePlacer
+    public class ForceDirectedCSR : BasePlacer
     {
         public ForceDirectedCSR(ISettings settings) : base(settings)
         {
-            
         }
 
 
-       private static double Distance(Coordinate a, Coordinate b)
+        private static double Distance(Coordinate a, Coordinate b)
         {
             return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
         }
@@ -24,19 +23,18 @@ namespace Buddy.Placer
             return Math.Sqrt(Math.Pow(a.X, 2) + Math.Pow(a.Y, 2));
         }
 
-       
+
         private static Coordinate FindForceVector(Coordinate a, Coordinate b, double fourceModule)
         {
             Coordinate newCoord;
-            if ((Math.Abs(a.X - b.X) <double.Epsilon) && (Math.Abs(a.Y - b.Y) < double.Epsilon))
+            if ((Math.Abs(a.X - b.X) < double.Epsilon) && (Math.Abs(a.Y - b.Y) < double.Epsilon))
             {
-                Random r = new Random();
-                 newCoord = new Coordinate
+                var r = new Random();
+                newCoord = new Coordinate
                 {
-                    X = r.NextDouble() ,
-                    Y = r.NextDouble() 
+                    X = r.NextDouble(),
+                    Y = r.NextDouble()
                 };
-
             }
             else
             {
@@ -46,9 +44,9 @@ namespace Buddy.Placer
                     Y = b.Y - a.Y
                 };
             }
-            var norma = Norma(newCoord);
-            newCoord.X = (newCoord.X / norma) * fourceModule;
-            newCoord.Y = (newCoord.Y / norma) * fourceModule;
+            double norma = Norma(newCoord);
+            newCoord.X = (newCoord.X/norma)*fourceModule;
+            newCoord.Y = (newCoord.Y/norma)*fourceModule;
 
             return newCoord;
         }
@@ -57,15 +55,14 @@ namespace Buddy.Placer
         private static void CulcAttractiveForces(IGraph graph, IList<Coordinate> coordinates,
             IList<Coordinate> vectors)
         {
-            for (var i = 0; i < graph.XAdj.Length-1; i++)
+            for (int i = 0; i < graph.XAdj.Length - 1; i++)
             {
-                for (var k = graph.XAdj[i]; k < graph.XAdj[i+1]; k++)
+                for (int k = graph.XAdj[i]; k < graph.XAdj[i + 1]; k++)
                 {
-                    
-                    var j = graph.Adjency[k];
+                    int j = graph.Adjency[k];
                     if (j < i) continue;
-                    var attrForce = Distance(coordinates[i], coordinates[j]) * graph.Weights[k];
-                    var u = FindForceVector(coordinates[i], coordinates[j], attrForce);
+                    double attrForce = Distance(coordinates[i], coordinates[j])*graph.Weights[k];
+                    Coordinate u = FindForceVector(coordinates[i], coordinates[j], attrForce);
                     vectors[i].X += u.X;
                     vectors[i].Y += u.Y;
                     vectors[j].X -= u.X;
@@ -77,24 +74,25 @@ namespace Buddy.Placer
         private static void CulcRepulsiveForces(IGraph graph, IList<Coordinate> coordinates,
             IList<Coordinate> vectors, Size size)
         {
-           // INeighbor g = new NeighborTester.NeighborGraph(graph, size, coordinates);
+            // INeighbor g = new NeighborTester.NeighborGraph(graph, size, coordinates);
             double RepFoce;
-            for (var i = 0; i < graph.VerticesAmount; i++)
-            {//  var neighbors = g.Neighborhood(coordinates[i], i);
-             //  var etr = neighbors.GetEnumerator;
-               for (var j = i + 1; j < graph.VerticesAmount; j++)
-            //   while(etr.MoveNext())
+            for (int i = 0; i < graph.VerticesAmount; i++)
+            {
+//  var neighbors = g.Neighborhood(coordinates[i], i);
+                //  var etr = neighbors.GetEnumerator;
+                for (int j = i + 1; j < graph.VerticesAmount; j++)
+                    //   while(etr.MoveNext())
                 {
-                   // var j = etr.Current;
-                    if (Math.Abs(coordinates[i].X - coordinates[j].X) < double.Epsilon && Math.Abs(coordinates[i].Y - coordinates[j].Y) < double.Epsilon)
-                        RepFoce = -(graph.Radiuses[i]+graph.Radiuses[j]);
-                    else RepFoce = -1 / Distance(coordinates[i], coordinates[j]);
-                    var u = FindForceVector(coordinates[i], coordinates[j], RepFoce);
+                    // var j = etr.Current;
+                    if (Math.Abs(coordinates[i].X - coordinates[j].X) < double.Epsilon &&
+                        Math.Abs(coordinates[i].Y - coordinates[j].Y) < double.Epsilon)
+                        RepFoce = -(graph.Radiuses[i] + graph.Radiuses[j]);
+                    else RepFoce = -1/Distance(coordinates[i], coordinates[j]);
+                    Coordinate u = FindForceVector(coordinates[i], coordinates[j], RepFoce);
                     vectors[i].X += u.X;
                     vectors[i].Y += u.Y;
                     vectors[j].X -= u.X;
                     vectors[j].Y -= u.Y;
-
                 }
             }
         }
@@ -106,45 +104,43 @@ namespace Buddy.Placer
 
         private static double ReductionCoef(Size size, IGraph graph, IList<Coordinate> vectors)
         {
-            var maxModule = vectors.Max(v => Norma(v));
-            return MaxStep(size, graph) / maxModule;
+            double maxModule = vectors.Max(v => Norma(v));
+            return MaxStep(size, graph)/maxModule;
         }
 
         private static void CulcGradient(IGraph graph, IList<Coordinate> coordinates, Coordinate[] gradient, Size size)
         {
-            for (var i = 0; i < graph.VerticesAmount; i++)
+            for (int i = 0; i < graph.VerticesAmount; i++)
             {
                 gradient[i].X = 0;
                 gradient[i].Y = 0;
             }
             CulcAttractiveForces(graph, coordinates, gradient);
             CulcRepulsiveForces(graph, coordinates, gradient, size);
-            var r1 = ReductionCoef(size, graph, gradient);
-            foreach (var v in gradient)
+            double r1 = ReductionCoef(size, graph, gradient);
+            foreach (Coordinate v in gradient)
             {
-                v.X = v.X * r1;
-                v.Y = v.Y * r1;
+                v.X = v.X*r1;
+                v.Y = v.Y*r1;
             }
         }
 
         public static void Scale(Size size, IList<Coordinate> coordinates, IGraph graph, bool always = false)
         {
+            double maxX = coordinates[0].X;
+            double minX = coordinates[0].Y;
+            double maxY = coordinates[0].X;
+            double minY = coordinates[0].Y;
 
-            var maxX = coordinates[0].X;
-            var minX = coordinates[0].Y;
-            var maxY = coordinates[0].X;
-            var minY = coordinates[0].Y;
-
-            for (var i = 1; i < coordinates.Count; i++)
+            for (int i = 1; i < coordinates.Count; i++)
             {
                 if (coordinates[i].X > maxX) maxX = coordinates[i].X;
                 if (coordinates[i].Y > maxY) maxY = coordinates[i].Y;
                 if (coordinates[i].X < minX) minX = coordinates[i].X;
                 if (coordinates[i].Y < minY) minY = coordinates[i].Y;
-
             }
 
-            for (var i = 0; i < graph.VerticesAmount; i++)
+            for (int i = 0; i < graph.VerticesAmount; i++)
             {
                 if (coordinates[i].X + graph.Radiuses[i] > maxX) maxX = coordinates[i].X + graph.Radiuses[i];
                 if (coordinates[i].Y + graph.Radiuses[i] > maxY) maxY = coordinates[i].Y + graph.Radiuses[i];
@@ -153,34 +149,32 @@ namespace Buddy.Placer
             }
             if ((maxX > size.Width) || (maxY > size.Height) || (minX < 0) || (minY < 0) || always)
             {
-                var weight = maxX - minX;
-                var height = maxY - minY;
+                double weight = maxX - minX;
+                double height = maxY - minY;
 
-                var coeffx = (size.Width - 20) / weight;
-                var coeffy = (size.Height - 20) / height;
-                for (var i = 0; i < graph.VerticesAmount; i++)
+                double coeffx = (size.Width - 20)/weight;
+                double coeffy = (size.Height - 20)/height;
+                for (int i = 0; i < graph.VerticesAmount; i++)
                 {
-                    coordinates[i].X = (coordinates[i].X - minX) * coeffx + 10;
-                    coordinates[i].Y = (coordinates[i].Y - minY) * coeffy + 10;
-
+                    coordinates[i].X = (coordinates[i].X - minX)*coeffx + 10;
+                    coordinates[i].Y = (coordinates[i].Y - minY)*coeffy + 10;
                 }
             }
-
         }
 
 
         public override IList<Coordinate> PlaceGraph(IGraph graph, IList<Coordinate> coordinate, Size size)
         {
             IList<Coordinate> newCoord = coordinate.ToList();
-            var iterations = Settings.Iterations;
-            Coordinate[] gradient = new Coordinate[graph.VerticesAmount];
-            for (var i = 0; i < gradient.Length; i++)
+            int iterations = Settings.Iterations;
+            var gradient = new Coordinate[graph.VerticesAmount];
+            for (int i = 0; i < gradient.Length; i++)
                 gradient[i] = new Coordinate(0, 0);
             do
             {
                 CulcGradient(graph, newCoord, gradient, size);
 
-                for (var i = 0; i < graph.VerticesAmount; i++)
+                for (int i = 0; i < graph.VerticesAmount; i++)
                 {
                     newCoord[i].X += gradient[i].X;
                     newCoord[i].Y += gradient[i].Y;
--- Buddy.All/Buddy.Placer/MultilevelPlaсer.cs
+++ Buddy.All/Buddy.Placer/MultilevelPlaсer.cs
@@ -1,44 +1,45 @@
-﻿using Buddy.Common.Printers;
-using Buddy.Common.Structures;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using Buddy.Common.Printers;
+using Buddy.Common.Structures;
 
 namespace Buddy.Placer
 {
     public class MultilevelPlaсer : BasePlacer
     {
         private readonly IPlacer m_localPlacer;
-    
+
         public MultilevelPlaсer(ISettings settings, IPlacer localPlacer)
             : base(settings)
         {
             m_localPlacer = localPlacer;
         }
-    
+
         public override IList<Coordinate> PlaceGraph(IGraph symmetricGraph, IList<Coordinate> coordinate, Size size)
         {
             double width = size.Width;
             double height = size.Height;
-            var initialX = coordinate.Select(c => c.X).ToArray();
-            var initialY = coordinate.Select(c => c.Y).ToArray();
+            double[] initialX = coordinate.Select(c => c.X).ToArray();
+            double[] initialY = coordinate.Select(c => c.Y).ToArray();
             double[] localX = initialX.ToArray();
             double[] localY = initialY.ToArray();
 
             double[] localresultX;
             double[] localresultY;
-            var ite = Settings.Iterations;
+            int ite = Settings.Iterations;
             do
             {
-                PlaceGraph(symmetricGraph.VerticesAmount, symmetricGraph.Radiuses, symmetricGraph.XAdj, symmetricGraph.Adjency, symmetricGraph.Weights, width,
-                height, localX, localY, out localresultX, out localresultY);
+                PlaceGraph(symmetricGraph.VerticesAmount, symmetricGraph.Radiuses, symmetricGraph.XAdj,
+                    symmetricGraph.Adjency, symmetricGraph.Weights, width,
+                    height, localX, localY, out localresultX, out localresultY);
 
                 localX = localresultX.ToArray();
                 localY = localresultY.ToArray();
             } while (--ite > 0);
 
-            
+
             double[] resultX;
             double[] resultY;
 
@@ -46,7 +47,7 @@ namespace Buddy.Placer
             resultY = localY.ToArray();
             //PlaceGraph(symmetricGraph.VerticesAmount, symmetricGraph.Radiuses, symmetricGraph.XAdj, symmetricGraph.Adjency, symmetricGraph.Weights, width,
             //    height, initialX, initialY, out resultX, out resultY);
-    
+
             return resultX.Select((t, i) => new Coordinate(t, resultY[i])).ToList();
         }
 
@@ -54,14 +55,15 @@ namespace Buddy.Placer
         {
             double width = size.Width;
             double height = size.Height;
-            var initialX = coordinate.Select(c => c.X).ToArray();
-            var initialY = coordinate.Select(c => c.Y).ToArray();
+            double[] initialX = coordinate.Select(c => c.X).ToArray();
+            double[] initialY = coordinate.Select(c => c.Y).ToArray();
             double[] resultX;
             double[] resultY;
-        
-            PlaceGraphIter(symmetricGraph.VerticesAmount, symmetricGraph.Radiuses, symmetricGraph.XAdj, symmetricGraph.Adjency, symmetricGraph.Weights, width,
+
+            PlaceGraphIter(symmetricGraph.VerticesAmount, symmetricGraph.Radiuses, symmetricGraph.XAdj,
+                symmetricGraph.Adjency, symmetricGraph.Weights, width,
                 height, initialX, initialY, out resultX, out resultY);
-        
+
             return resultX.Select((t, i) => new Coordinate(t, resultY[i])).ToList();
         }
 
@@ -69,104 +71,95 @@ namespace Buddy.Placer
             double[] weights, double width,
             double height, double[] initialX, double[] initialY, out double[] resultX, out double[] resultY)
         {
-
             PlaceGraphIter(nodes, radiuses, xAdj, adjency, weights, width, height, initialX, initialY, out resultX,
-                    out resultY);
+                out resultY);
         }
 
         public void PlaceGraphIter(int nodes, double[] radiuses, int[] xAdj, int[] adjency,
             double[] weights, double width,
             double height, double[] initialX, double[] initialY, out double[] resultX, out double[] resultY)
         {
-            IGraph graph = new Graph(nodes, xAdj, adjency, radiuses, weights);//nodes, rowIndexes[nodes], radiuses, weights, columnIndexes, rowIndexes);
-    
+            IGraph graph = new Graph(nodes, xAdj, adjency, radiuses, weights);
+                //nodes, rowIndexes[nodes], radiuses, weights, columnIndexes, rowIndexes);
+
             var dec = new GraphReducer(graph);
-            var labels = GetReduceLabels(nodes, graph).ToArray();
-    
-            var rgraph = dec.Reduce(labels.Select(x => x).ToArray());
-    
+            int[] labels = GetReduceLabels(nodes, graph).ToArray();
+
+            IGraph rgraph = dec.Reduce(labels.Select(x => x).ToArray());
+
             double[] localInitialX;
             double[] localInitialY;
-    
-            ComputePositions(nodes, initialX, initialY, rgraph.VerticesAmount, labels, out localInitialX, out localInitialY);
-    
+
+            ComputePositions(nodes, initialX, initialY, rgraph.VerticesAmount, labels, out localInitialX,
+                out localInitialY);
+
             double[] localResultX = localInitialX.ToArray();
             double[] localResultY = localInitialY.ToArray();
-    
+
             //m_localPlacer.PlaceGraph(rgraph.VerticesAmount, rgraph.Radiuses, rgraph.XAdj, rgraph.Adjency,
             //    rgraph.Weights, width, height, localInitialX, localInitialY, out localResultX, out localResultY);
-    
-            Drawer.DrawGraph(new Size((int)width, (int)height), rgraph,
-                localResultX.Select((t, i) => new Coordinate(t, localResultY[i])).ToList(), string.Format("multulevel_down_{0}.bmp", nodes));
-    
+
+            Drawer.DrawGraph(new Size((int) width, (int) height), rgraph,
+                localResultX.Select((t, i) => new Coordinate(t, localResultY[i])).ToList(),
+                string.Format("multulevel_down_{0}.bmp", nodes));
+
             if (rgraph.VerticesAmount > 50)
             {
-                var res = PlaceGraphIter(rgraph, localResultX.Select((x, i) => new Coordinate(x, localResultY[i])).ToList(),
-                    new Size((int)width, (int)height));
-    
+                IList<Coordinate> res = PlaceGraphIter(rgraph,
+                    localResultX.Select((x, i) => new Coordinate(x, localResultY[i])).ToList(),
+                    new Size((int) width, (int) height));
+
                 localResultX = res.Select(c => c.X).ToArray();
                 localResultY = res.Select(c => c.Y).ToArray();
             }
-    
+
             resultX = new double[nodes];
             resultY = new double[nodes];
             var rnd = new Random();
+
             for (int i = 0; i < nodes; i++)
             {
-                var j = labels[i];
-                resultX[i] = localResultX[j] + rnd.Next((int)(2 * rgraph.Radiuses[j])) - rgraph.Radiuses[j];
-    
-                resultY[i] = localResultY[j] + rnd.Next((int)(2 * rgraph.Radiuses[j])) - rgraph.Radiuses[j];
-    
+                int j = labels[i];
+                resultX[i] = localResultX[j] + rnd.Next((int) (2*rgraph.Radiuses[j])) - rgraph.Radiuses[j];
+
+                resultY[i] = localResultY[j] + rnd.Next((int) (2*rgraph.Radiuses[j])) - rgraph.Radiuses[j];
+
                 if (resultX[i] < 0)
                     resultX[i] = -resultX[i];
-    
+
                 if (resultY[i] < 0)
                     resultY[i] = -resultY[i];
-    
+
                 resultX[i] = Math.Min(width, resultX[i]);
                 resultY[i] = Math.Min(height, resultY[i]);
             }
-    
-            if (true)
-            {
-                var xx = resultX.ToArray();
-                var yy = resultY.ToArray();
-                //m_localPlacer.Settings.Iterations *= 2;
-                m_localPlacer.PlaceGraph(graph.VerticesAmount, graph.Radiuses, graph.XAdj, graph.Adjency,
-                    graph.Weights, width, height, xx, yy, out xx, out yy);
-                
-                if (yy.Any(double.IsNaN))
-                {
-                    throw new Exception();
-                }
-                
-                if (xx.Any(double.IsNaN))
-                {
-                    throw new Exception();
-                }
-    
-                Drawer.DrawGraph(new Size((int)width, (int)height), graph,
-                    xx.Select((t, i) => new Coordinate(t, yy[i])).ToList(),
-                    string.Format("multulevel_up_{0}.bmp", nodes));
-    
-                resultX = xx.ToArray();
-                resultY = yy.ToArray();
-            }
+
+            double[] xx = resultX.ToArray();
+            double[] yy = resultY.ToArray();
+
+            m_localPlacer.PlaceGraph(graph.VerticesAmount, graph.Radiuses, graph.XAdj, graph.Adjency,
+                graph.Weights, width, height, xx, yy, out xx, out yy);
+
+            Drawer.DrawGraph(new Size((int) width, (int) height), graph,
+                xx.Select((t, i) => new Coordinate(t, yy[i])).ToList(),
+                string.Format("multulevel_up_{0}.bmp", nodes));
+
+            resultX = xx.ToArray();
+            resultY = yy.ToArray();
         }
-    
+
         private static void ComputePositions(int nodes, double[] initialX, double[] initialY, int count, int[] labels,
             out double[] localInitialX, out double[] localInitialY)
         {
             localInitialX = new double[count];
             localInitialY = new double[count];
-    
+
             for (int i = 0; i < nodes; i++)
             {
-                var local = labels[i];
-                var amount = labels.Where(l => l == local).Count();
+                int local = labels[i];
+                int amount = labels.Where(l => l == local).Count();
                 var vertices = new int[amount];
-                var index = 0;
+                int index = 0;
                 for (int k = 0; k < labels.Length; k++)
                 {
                     if (labels[k] == local)
@@ -176,35 +169,35 @@ namespace Buddy.Placer
                 }
                 localInitialX[local] = 0;
                 localInitialY[local] = 0;
-    
-                foreach (var vertex in vertices)
+
+                foreach (int vertex in vertices)
                 {
                     localInitialX[local] += initialX[vertex];
                     localInitialY[local] += initialY[vertex];
                 }
-    
+
                 localInitialX[local] /= vertices.Length;
                 localInitialY[local] /= vertices.Length;
             }
         }
-    
+
         private static IEnumerable<int> GetReduceLabels(int nodes, IGraph symmetricGraph)
         {
             var labels = new int[nodes];
-    
+
             for (int i = 0; i < nodes; i++)
             {
                 labels[i] = -1;
             }
-    
-            var current = 0;
 
-            foreach (var vertex in symmetricGraph.Vertices)
+            int current = 0;
+
+            foreach (int vertex in symmetricGraph.Vertices)
             {
-                foreach (var label in symmetricGraph.SymAdj(vertex).OrderBy(e=>-e))
+                foreach (int label in symmetricGraph.SymAdj(vertex).OrderBy(e => -e))
                 {
-                    var first = vertex;
-                    var second = label;
+                    int first = vertex;
+                    int second = label;
 
                     if (labels[first] == -1 && labels[second] == -1)
                     {
@@ -215,13 +208,13 @@ namespace Buddy.Placer
                     }
                 }
             }
-    
-            for (var i = 0; i < nodes; i++)
+
+            for (int i = 0; i < nodes; i++)
             {
                 if (labels[i] == -1)
                     labels[i] = current++;
             }
-    
+
             return labels;
         }
     }
